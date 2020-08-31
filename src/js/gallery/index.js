dd280ae03bf18ebb4bb19e81405d6fda02af4549
Joshua Fielding <joshuafielding@Joshuas-MacBook-Pro.local>
2020-09-01 08:57:30 +1000

    added chapter2 block

---
+++ src/js/gallery/index.js
@@ -0,0 +1,7 @@
+const galleryItem = document.querySelectorAll(".nav-item");
+const projectOne = document.querySelector(".project-one");
+const projectTwo = document.querySelector(".project-two");
+const projectThree = document.querySelector(".project-three");
+const projectFour = document.querySelector(".project-four");
+
+const projects = document.querySelectorAll(".project");
