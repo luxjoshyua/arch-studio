0aac26fa2b97d8a5e2e830ba3dfc53d6c8a0e9db
Joshua Fielding <joshuafielding@Joshuas-MacBook-Pro.local>
2020-09-05 16:34:10 +1000

    got gallery working, needs to be massively refactored

--- src/js/gallery/index.js
+++ src/js/gallery/index.js
@@ -1,7 +1,64 @@
-const galleryItem = document.querySelectorAll(".nav-item");
+const galleryItem = document.querySelectorAll(".nav-item span");
 const projectOne = document.querySelector(".project-one");
 const projectTwo = document.querySelector(".project-two");
 const projectThree = document.querySelector(".project-three");
 const projectFour = document.querySelector(".project-four");
 
-const projects = document.querySelectorAll(".project");
+const projects = document.querySelectorAll(".hero .project");
+const navBtns = document.querySelectorAll(".hero .nav-btn");
+
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", (e) => {
+  projects.forEach((project) => {
+    project.classList.add("project-inactive");
+    project.classList.remove("project-active");
+  });
+  navBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  projectOne.classList.toggle("project-active");
+  projectOne.classList.remove("project-inactive");
+  btn1.classList.add("active");
+});
+
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", (e) => {
+  projects.forEach((project) => {
+    project.classList.add("project-inactive");
+    project.classList.remove("project-active");
+  });
+  navBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  projectTwo.classList.toggle("project-active");
+  projectTwo.classList.remove("project-inactive");
+  btn2.classList.add("active");
+});
+
+const btn3 = document.getElementById("btn3");
+btn3.addEventListener("click", (e) => {
+  projects.forEach((project) => {
+    project.classList.add("project-inactive");
+    project.classList.remove("project-active");
+  });
+  navBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  projectThree.classList.toggle("project-active");
+  projectThree.classList.remove("project-inactive");
+  btn3.classList.add("active");
+});
+
+const btn4 = document.getElementById("btn4");
+btn4.addEventListener("click", (e) => {
+  projects.forEach((project) => {
+    project.classList.add("project-inactive");
+    project.classList.remove("project-active");
+  });
+  navBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  projectFour.classList.toggle("project-active");
+  projectFour.classList.remove("project-inactive");
+  btn4.classList.add("active");
+});
