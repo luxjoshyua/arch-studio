dc0bb573d7f6884f5defff607e27a47a0637c362
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-09-11 15:07:00 +1000

    gallery tweaks

--- src/js/gallery/index.js
+++ src/js/gallery/index.js
@@ -10,26 +10,25 @@ if (projects[0]) {
   projects[0].classList.add("project-active");
 }
 if (navBtns[0]) {
-  navBtns[0].classList.add("active");
+  navBtns[0].classList.toggle("active");
 }
 
 navBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let target = e.target.innerHTML;
-    // console.log(e);
+    btn.classList.toggle("active");
+
+    // btn.nextElementSibling.classList.remove("active");
+    // btn.previousElementSibling.classList.remove("active");
+
     projects.forEach((project) => {
       project.classList.remove("project-active");
-      //   project.classList.add("project-inactive");
 
       if (target === "01") {
         if (project.classList.contains("project-one")) {
           project.classList.add("project-active");
           project.classList.remove("project-inactive");
         }
-        document.getElementById("btn1").classList.add("active");
-        document.getElementById("btn2").classList.remove("active");
-        document.getElementById("btn3").classList.remove("active");
-        document.getElementById("btn4").classList.remove("active");
       }
 
       if (target === "02") {
@@ -37,11 +36,6 @@ navBtns.forEach((btn) => {
           project.classList.add("project-active");
           project.classList.remove("project-inactive");
         }
-
-        document.getElementById("btn2").classList.add("active");
-        document.getElementById("btn1").classList.remove("active");
-        document.getElementById("btn3").classList.remove("active");
-        document.getElementById("btn4").classList.remove("active");
       }
 
       if (target === "03") {
@@ -49,10 +43,6 @@ navBtns.forEach((btn) => {
           project.classList.add("project-active");
           project.classList.remove("project-inactive");
         }
-        document.getElementById("btn3").classList.add("active");
-        document.getElementById("btn1").classList.remove("active");
-        document.getElementById("btn2").classList.remove("active");
-        document.getElementById("btn4").classList.remove("active");
       }
 
       if (target === "04") {
@@ -60,10 +50,6 @@ navBtns.forEach((btn) => {
           project.classList.add("project-active");
           project.classList.remove("project-inactive");
         }
-        document.getElementById("btn4").classList.add("active");
-        document.getElementById("btn1").classList.remove("active");
-        document.getElementById("btn3").classList.remove("active");
-        document.getElementById("btn2").classList.remove("active");
       }
     });
   });
